192a2c50cbc371ce8dca6497e6346723355bf39b
kafka <rachilin@fiim.ltd>
2021-02-07 11:58:51 +0300

    fixed conference controller

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,6 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
@@ -17,16 +20,26 @@ class ConferenceController extends AbstractController
      * @param Environment $twig
      * @param ConferenceRepository $conferenceRepository
      * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        return new Response($twig->render('conference/index.html', [
+        return new Response($twig->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]));
     }
 
     /**
      * @Route("/conference/{id}", name="conference")
+     * @param Environment $twig
+     * @param Conference $conference
+     * @param CommentRepository $commentRepository
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
     {
