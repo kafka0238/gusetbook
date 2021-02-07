402528f6d0fa92304641085575b630d0588ac5e5
kafka <rachilin@fiim.ltd>
2021-02-07 14:53:29 +0300

    Add global conference to twig

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -2,17 +2,28 @@
 
 namespace App\EventSubscriber;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Twig\Environment;
 
 class TwigEventSubscriber implements EventSubscriberInterface
 {
-    public function onKernelController(ControllerEvent $event)
+    private $twig;
+    private $conferenceRepository;
+
+    public function __construct(Environment $twig, ConferenceRepository $conferenceRepository)
     {
-        // ...
+        $this->twig = $twig;
+        $this->conferenceRepository = $conferenceRepository;
     }
 
-    public static function getSubscribedEvents()
+    public function onKernelController(ControllerEvent $event)
+    {
+        $this->twig->addGlobal('conferences', $this->conferenceRepository->findAll());
+    }
+
+    public static function getSubscribedEvents(): array
     {
         return [
             'kernel.controller' => 'onKernelController',
